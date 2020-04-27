c6f8cd9150efadf58e6e38f1f52ab7486856284f
AlexandrDev <alexandros96@yandex.ru>
2020-04-27 07:54:17 +0600

    restructure

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
 const gulp = require('gulp');
+const pug = require('gulp-pug');
 const sass = require('gulp-sass');
 const gcmq = require('gulp-group-css-media-queries');
 
-function style() {
-    return gulp.src('scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gcmq())
+
+function buildHTML() {
+    return gulp.src('src/pug/*.pug')
+        .pipe(pug({pretty: true}))
         .pipe(gulp.dest('dist'))
 }
 
-function watch() {
-    gulp.watch('scss/**/*.scss', style)
+function style() {
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gcmq())
+        .pipe(gulp.dest('dist/css'))
 }
 
-exports.default = gulp.series(style, watch);
+function watch() {
+    gulp.watch('src/pug/**/*.pug', buildHTML)
+    gulp.watch('src/scss/**/*.scss', style)
+}
+
+exports.default = gulp.series(buildHTML, style, watch);
