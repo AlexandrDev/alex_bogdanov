ee8a519f6d9422bbbf38d5853533afd3b3a0e1c3
AlexandrDev <alexandros96@yandex.ru>
2020-05-01 00:33:44 +0600

    working on header

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,18 +1,15 @@
 $(function() {
     console.log( "ready!" );
 
-    // $(document).mouseup(function(e) { 
-    //     let dd_menu = $(".main-menu__dropdown");
-        
-	// 	if (!dd_menu.is(e.target) && dd_menu.has(e.target).length === 0) {
-	// 		$('.main-menu__link').removeClass('opened');
-	// 	}
-	// });
+    $(".dropdown-toggle").dropdown();
 
-
-    $('.main-menu__link.has-child > a').click(function(e) {       
-        e.preventDefault();
-
-        $(this).parent().toggleClass('opened');
+    $('.hamburger').click(function() {
+        $('header').toggleClass('menu-opened')
     });
+
+    // $('.main-menu__link.has-child > a').click(function(e) {       
+    //     e.preventDefault();
+
+    //     $(this).parent().toggleClass('opened');
+    // });
 });
