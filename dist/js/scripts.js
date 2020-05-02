4cf3552e7abc1aaa8fa6f8cd3732945543885593
AlexandrDev <alexandros96@yandex.ru>
2020-05-03 00:06:02 +0600

    home: slider (not adaptive)

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,10 +1,17 @@
 $(function() {
     console.log( "ready!" );
 
-    $(".dropdown-toggle").dropdown();
+    $("[data-toggle='dropdown']").dropdown();
 
     $('.hamburger').click(function() {
         $('header').toggleClass('menu-opened')
+    });
+
+
+    $('#fw-slider').flickity({
+        wrapAround: true,
+        prevNextButtons: false,
+        pageDots: false
     });
 
     // $('.main-menu__link.has-child > a').click(function(e) {       
