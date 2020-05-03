bfd81124df971d77cfe88d11a442071180c296a4
AlexandrDev <alexandros96@yandex.ru>
2020-05-04 02:09:10 +0600

    add prod-card, anymore blocks on homepage

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,10 @@
 $(function() {
     console.log( "ready!" );
 
+    $(window).resize(function() {
+        setViewport()
+    });
+
     $("[data-toggle='dropdown']").dropdown();
 
     $('.hamburger').click(function() {
@@ -11,7 +15,33 @@ $(function() {
     $('#fw-slider').flickity({
         wrapAround: true,
         prevNextButtons: false,
-        pageDots: false
+        pageDots: false,
+        adaptiveHeight: true
+    });
+
+
+    $('.product-card').each(function() {
+        let $thumb = $(this).find('.product-card__thumb'),
+            $btns = $thumb.find('.product-card__buttons');
+
+        $thumb.hover(
+            function() {
+                $btns.addClass('show');
+            },
+            function() {
+                $btns.removeClass('show');
+            }
+        );
+
+        $btns.hover(
+            function() {
+                $btns.addClass('show_sizes');
+            },
+            function() {
+                $btns.removeClass('show_sizes');
+            }
+        );
+
     });
 
 });
