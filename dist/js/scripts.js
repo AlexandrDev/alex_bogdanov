27ceab1e23af429e9128018ae1e0e698e9899802
AlexandrDev <alexandros96@yandex.ru>
2020-05-04 23:27:05 +0600

    add mob menu

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,14 +1,50 @@
 $(function() {
-    console.log( "ready!" );
 
     $(window).resize(function() {
         setViewport()
+        set_sticky_header()
     });
+
+    $(window).scroll(function() {
+        set_sticky_header()
+    });
+
+
+    // stisky-header
+    let header_h = $('header').height(),
+        header_top_h = $('.header__top').height();
+        
+    set_sticky_header();
+
+    function set_sticky_header() {
+        if ($(window).scrollTop() >= header_top_h) {
+            $('header').addClass("sticky-header");
+        } else {
+            $('header').removeClass("sticky-header");
+        }
+    }
+    
 
     $("[data-toggle='dropdown']").dropdown();
 
+
     $('.hamburger').click(function() {
-        $('header').toggleClass('menu-opened')
+        if ($('header').hasClass('menu-opened') ) {
+            $('.mobile-menu-parent').removeClass('expanded');
+        }
+        $('header').toggleClass('menu-opened');
+    });
+
+
+    let navExpand = [].slice.call(document.querySelectorAll('.mobile-menu-parent'));
+
+    navExpand.forEach((item) => {
+        item.querySelector('a').addEventListener('click', (e) => {
+            e.preventDefault();
+            $('.mobile-menu__content').stop().animate({scrollTop:0}, 200, 'swing');
+            item.classList.add('expanded');
+        });
+        item.querySelector('.mobile-menu-back').addEventListener('click', () => item.classList.remove('expanded'));
     });
 
 
